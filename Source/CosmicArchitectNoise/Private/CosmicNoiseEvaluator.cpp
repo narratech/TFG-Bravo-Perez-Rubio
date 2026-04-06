79bc2789ea3cef8cc6536fbd3e1963fa1003dfaf
JaviBravoPerucho <125208949+JaviBravoPerucho@users.noreply.github.com>
2026-04-06 16:14:23 +0200

    Refactorizacion de ruido para no repetir codigo

---
+++ Source/CosmicArchitectNoise/Private/CosmicNoiseEvaluator.cpp
@@ -0,0 +1,200 @@
+#include "CosmicNoiseEvaluator.h"
+
+FCosmicNoiseEvaluator::FCosmicNoiseEvaluator(const FCosmicNoiseGenerationParameters& InSettings)
+    : Settings(InSettings)
+{
+    // 1. CONFIGURAR CLIMA Y WARP GLOBALES
+    HumidityNoise.SetSeed(Settings.Seed);
+    HumidityNoise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+    HumidityNoise.SetFractalType(FastNoiseLite::FractalType_FBm);
+    HumidityNoise.SetFrequency(Settings.HumidityFrequency * 100.0f);
+    HumidityNoise.SetFractalOctaves(Settings.HumidityOctaves);
+
+    TempNoise.SetSeed(Settings.Seed);
+    TempNoise.SetFrequency(Settings.TemperatureFrequency * 100.0f);
+
+    if (Settings.bUseDomainWarp)
+    {
+        WarpNoise.SetSeed(Settings.Seed + 1337);
+        WarpNoise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+        WarpNoise.SetFrequency(Settings.DomainWarpFrequency);
+    }
+
+    if (Settings.bIsCraterPlanet)
+    {
+        CraterNoise.SetSeed(Settings.Seed + 4242);
+        CraterNoise.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
+        CraterNoise.SetFrequency(Settings.CraterFrequency);
+        CraterNoise.SetFractalType(FastNoiseLite::FractalType_None);
+        CraterNoise.SetCellularDistanceFunction(FastNoiseLite::CellularDistanceFunction_Euclidean);
+        CraterNoise.SetCellularReturnType(FastNoiseLite::CellularReturnType_Distance);
+    }
+
+    // 2. CONFIGURAR MATRIZ DE BIOMAS
+    BiomeNoises.SetNum(Settings.Biomes.Num());
+    MaxPossibleHeight = 0.0f;
+
+    for (int32 b = 0; b < Settings.Biomes.Num(); b++)
+    {
+        float BiomeMaxHeight = 0.0f;
+        const FCosmicBiomeData& BiomeData = Settings.Biomes[b];
+
+        BiomeNoises[b].Reserve(BiomeData.NoiseLayers.Num());
+
+        for (int32 l = 0; l < BiomeData.NoiseLayers.Num(); l++)
+        {
+            const FCosmicNoiseTypes& Layer = BiomeData.NoiseLayers[l];
+            FastNoiseLite Noise;
+
+            Noise.SetSeed(Settings.Seed + (b * 100) + l);
+
+            // TIPO DE RUIDO
+            switch (Layer.NoiseType) {
+            case ECosmicNoiseType::Perlin: Noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin); break;
+            case ECosmicNoiseType::Simplex:
+            case ECosmicNoiseType::Ridged: Noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2); break;
+            case ECosmicNoiseType::Cellular: Noise.SetNoiseType(FastNoiseLite::NoiseType_Cellular); break;
+            case ECosmicNoiseType::Value: Noise.SetNoiseType(FastNoiseLite::NoiseType_Value); break;
+            }
+
+            // TIPO DE FRACTAL
+            switch (Layer.FractalType) {
+            case ECosmicFractalType::None: Noise.SetFractalType(FastNoiseLite::FractalType_None); break;
+            case ECosmicFractalType::FBM: Noise.SetFractalType(FastNoiseLite::FractalType_FBm); break;
+            case ECosmicFractalType::Ridged: Noise.SetFractalType(FastNoiseLite::FractalType_Ridged); break;
+            case ECosmicFractalType::PingPong: Noise.SetFractalType(FastNoiseLite::FractalType_PingPong); break;
+            }
+
+            Noise.SetFrequency(Layer.Frequency);
+            Noise.SetFractalOctaves(Layer.Octaves);
+            Noise.SetFractalLacunarity(Layer.Lacunarity);
+            Noise.SetFractalGain(Layer.Persistence);
+
+            BiomeNoises[b].Add(Noise);
+            BiomeMaxHeight += Layer.Amplitude;
+        }
+
+        MaxPossibleHeight = FMath::Max(MaxPossibleHeight, BiomeMaxHeight);
+    }
+
+    if (MaxPossibleHeight == 0.0f) MaxPossibleHeight = 1000.0f;
+}
+
+void FCosmicNoiseEvaluator::EvaluatePoint(const FVector& NoiseDir, float& OutHeight, FLinearColor& OutColor)
+{
+    float X = NoiseDir.X;
+    float Y = NoiseDir.Y;
+    float Z = NoiseDir.Z;
+
+    // 1. DOMAIN WARP
+    if (Settings.bUseDomainWarp)
+    {
+        float WarpX = WarpNoise.GetNoise(X, Y, Z) * Settings.DomainWarpStrength;
+        float WarpY = WarpNoise.GetNoise(X + 31.7f, Y + 17.3f, Z + 47.1f) * Settings.DomainWarpStrength;
+        float WarpZ = WarpNoise.GetNoise(X + 59.2f, Y + 11.8f, Z + 23.4f) * Settings.DomainWarpStrength;
+        X += WarpX; Y += WarpY; Z += WarpZ;
+    }
+
+    // 2. CLIMA MATEMÁTICO (Para elegir bioma)
+    float RawHum = HumidityNoise.GetNoise(NoiseDir.X, NoiseDir.Y, NoiseDir.Z);
+    float FinalHum = (RawHum + 1.0f) * 0.5f + Settings.HumidityOffset;
+    FinalHum = FMath::Clamp((FinalHum - 0.5f) * Settings.HumidityContrast + 0.5f, 0.0f, 1.0f);
+
+    float Latitude = FMath::Abs(NoiseDir.Z);
+    float BaseTemp = 1.0f - (Latitude * Settings.LatitudeEffect);
+    float TempVariance = TempNoise.GetNoise(NoiseDir.X, NoiseDir.Y, NoiseDir.Z) * 0.2f;
+    float FinalTemp = FMath::Clamp(BaseTemp + TempVariance, 0.0f, 1.0f);
+
+    // 3. SELECCIÓN DE BIOMA
+    int32 ClosestBiomeIdx = -1;
+    int32 SecondClosestIdx = -1;
+    float MinDistSq = UE_BIG_NUMBER;
+    float SecondMinDistSq = UE_BIG_NUMBER;
+
+    for (int32 b = 0; b < Settings.Biomes.Num(); b++)
+    {
+        float dTemp = FinalTemp - Settings.Biomes[b].TargetTemperature;
+        float dHum = FinalHum - Settings.Biomes[b].TargetHumidity;
+        float DistSq = (dTemp * dTemp) + (dHum * dHum);
+
+        if (DistSq < MinDistSq)
+        {
+            SecondMinDistSq = MinDistSq;
+            SecondClosestIdx = ClosestBiomeIdx;
+            MinDistSq = DistSq;
+            ClosestBiomeIdx = b;
+        }
+        else if (DistSq < SecondMinDistSq)
+        {
+            SecondMinDistSq = DistSq;
+            SecondClosestIdx = b;
+        }
+    }
+
+    float FinalHeight = 0.0f;
+
+    if (ClosestBiomeIdx != -1)
+    {
+        float Height1 = 0.0f;
+        for (int32 l = 0; l < Settings.Biomes[ClosestBiomeIdx].NoiseLayers.Num(); l++) {
+            Height1 += BiomeNoises[ClosestBiomeIdx][l].GetNoise(X, Y, Z) * Settings.Biomes[ClosestBiomeIdx].NoiseLayers[l].Amplitude;
+        }
+
+        if (SecondClosestIdx != -1)
+        {
+            float Height2 = 0.0f;
+            for (int32 l = 0; l < Settings.Biomes[SecondClosestIdx].NoiseLayers.Num(); l++) {
+                Height2 += BiomeNoises[SecondClosestIdx][l].GetNoise(X, Y, Z) * Settings.Biomes[SecondClosestIdx].NoiseLayers[l].Amplitude;
+            }
+
+            float Dist1 = FMath::Sqrt(MinDistSq);
+            float Dist2 = FMath::Sqrt(SecondMinDistSq);
+            if (Dist1 < 0.0001f) Dist1 = 0.0001f;
+            if (Dist2 < 0.0001f) Dist2 = 0.0001f;
+
+            float Weight1 = 1.0f / Dist1;
+            float Weight2 = 1.0f / Dist2;
+            float SumWeights = Weight1 + Weight2;
+
+            FinalHeight = (Height1 * (Weight1 / SumWeights)) + (Height2 * (Weight2 / SumWeights));
+        }
+        else
+        {
+            FinalHeight = Height1;
+        }
+    }
+
+    // 4. CRÁTERES
+    if (Settings.bIsCraterPlanet)
+    {
+        float CellDistance = CraterNoise.GetNoise(X, Y, Z);
+        float CraterRadius = Settings.CraterRadiusMultiplier;
+        CellDistance = (CellDistance + 1.0f) * 0.5f;
+
+        if (CellDistance < CraterRadius * 1.3f)
+        {
+            float t = CellDistance / CraterRadius;
+            float craterHeight = 0.0f;
+
+            if (t < 1.0f)
+            {
+                float floorStart = Settings.CraterFloorHeight;
+                float bowl = (t < floorStart) ? 1.0f : FMath::Pow(1.0f - FMath::SmoothStep(0.0f, 1.0f, (t - floorStart) / (1.0f - floorStart)), 2.0f);
+                craterHeight -= bowl * Settings.CraterDepth;
+            }
+
+            float rim = FMath::Exp(-FMath::Pow((t - 1.0f) / 0.15f, 2.0f) * Settings.CraterRimSharpness);
+            craterHeight += rim * Settings.CraterDepth * Settings.CraterRimHeight;
+            FinalHeight += craterHeight;
+        }
+    }
+
+    // 5. SALIDA (Altura y Color)
+    OutHeight = FinalHeight;
+
+    float AltitudePenalty = FMath::Clamp(FinalHeight / MaxPossibleHeight, 0.0f, 1.0f);
+    float VisualTemp = FMath::Clamp(FinalTemp - (AltitudePenalty * Settings.AltitudeTemperaturePenalty), 0.0f, 1.0f);
+    float BiomeID = (ClosestBiomeIdx != -1) ? ((float)ClosestBiomeIdx / 255.0f) : 0.0f;
+
+    OutColor = FLinearColor(AltitudePenalty, VisualTemp, FinalHum, BiomeID);
+}
--- Source/CosmicArchitectNoise/Public/CosmicArchitectNoiseGenerator.h
+++ Source/CosmicArchitectNoise/Public/CosmicArchitectNoiseGenerator.h
@@ -5,6 +5,7 @@
 #include "ThirdParty/FastNoiseLite.h"
 #include "CosmicNoiseTypes.h"
 #include "CosmicNoiseSettings.h"
+#include "CosmicNoiseEvaluator.h"
 
 class FCosmicArchitectNoiseGenerator: public FNonAbandonableTask {
 public:
@@ -48,108 +49,7 @@ public:
     void DoWork()
     {
 
-        // Crear ruidos configurados una vez por capa
-        TArray<TArray<FastNoiseLite>> BiomeNoises;
-        BiomeNoises.SetNum(NoiseSettings.Biomes.Num());
-
-        float MaxPossibleHeight = 0.0f;
-
-        for (int i = 0; i < NoiseSettings.Biomes.Num(); i++)
-        {
-            float BiomeMaxHeight = 0.0f;
-            const FCosmicBiomeData& BiomeData = NoiseSettings.Biomes[i];
-
-            BiomeNoises[i].Reserve(BiomeData.NoiseLayers.Num());
-
-            for (int j = 0; j < BiomeData.NoiseLayers.Num(); j++) {
-                const FCosmicNoiseTypes& Layer = BiomeData.NoiseLayers[j];
-                FastNoiseLite Noise;
-                Noise.SetSeed(NoiseSettings.Seed + (i * 100) + j); //Semilla ligeramente distinta para no repetir
-
-                // Noise Type 
-                switch (Layer.NoiseType)
-                {
-                case ECosmicNoiseType::Perlin:
-                    Noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
-                    break;
-                case ECosmicNoiseType::Simplex:
-                case ECosmicNoiseType::Ridged: // Ridged usa Simplex como base
-                    Noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
-                    break;
-                case ECosmicNoiseType::Cellular:
-                    Noise.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
-                    break;
-                case ECosmicNoiseType::Value:
-                    Noise.SetNoiseType(FastNoiseLite::NoiseType_Value);
-                    break;
-                }
-
-                // Fractal Type 
-                switch (Layer.FractalType)
-                {
-                case ECosmicFractalType::None:
-                    Noise.SetFractalType(FastNoiseLite::FractalType_None);
-                    break;
-                case ECosmicFractalType::FBM:
-                    Noise.SetFractalType(FastNoiseLite::FractalType_FBm);
-                    break;
-                case ECosmicFractalType::Ridged:
-                    Noise.SetFractalType(FastNoiseLite::FractalType_Ridged);
-                    break;
-                case ECosmicFractalType::PingPong:
-                    Noise.SetFractalType(FastNoiseLite::FractalType_PingPong);
-                    break;
-                }
-
-                // Parámetros fractales
-                Noise.SetFrequency(Layer.Frequency);
-                Noise.SetFractalOctaves(Layer.Octaves);
-                Noise.SetFractalLacunarity(Layer.Lacunarity);
-                Noise.SetFractalGain(Layer.Persistence);
-
-                BiomeNoises[i].Add(Noise);
-                BiomeMaxHeight += Layer.Amplitude;
-            }
-            MaxPossibleHeight = FMath::Max(MaxPossibleHeight, BiomeMaxHeight);
-        }
-        if (MaxPossibleHeight == 0.0f) MaxPossibleHeight = 1000.0f;
-
-        //Temperatura y humedad
-        FastNoiseLite HumidityNoise;
-        HumidityNoise.SetSeed(NoiseSettings.Seed);
-        HumidityNoise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
-        HumidityNoise.SetFractalType(FastNoiseLite::FractalType_FBm);
-        HumidityNoise.SetFrequency(NoiseSettings.HumidityFrequency * 100.0f);
-        HumidityNoise.SetFractalOctaves(NoiseSettings.HumidityOctaves);
-
-        FastNoiseLite TempNoise;
-        TempNoise.SetSeed(NoiseSettings.Seed);
-        TempNoise.SetFrequency(NoiseSettings.TemperatureFrequency * 100.0f);
-
-        FastNoiseLite CraterNoise;
-
-        if (NoiseSettings.bIsCraterPlanet)
-        {
-            CraterNoise.SetSeed(NoiseSettings.Seed + 4242);
-            CraterNoise.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
-            CraterNoise.SetFrequency(NoiseSettings.CraterFrequency);
-            CraterNoise.SetFractalType(FastNoiseLite::FractalType_None);
-            //CraterNoise.SetFractalOctaves(CraterOctaves);
-            //CraterNoise.SetFractalLacunarity(CraterLacunarity);
-            //CraterNoise.SetFractalGain(CraterPersistence);
-            CraterNoise.SetCellularDistanceFunction(FastNoiseLite::CellularDistanceFunction_Euclidean);
-            CraterNoise.SetCellularReturnType(FastNoiseLite::CellularReturnType_Distance);
-
-        }
-
-        // Domain Warp (configurado UNA VEZ) 
-        FastNoiseLite WarpNoise;
-        if (NoiseSettings.bUseDomainWarp)
-        {
-            WarpNoise.SetSeed(NoiseSettings.Seed + 1337);
-            WarpNoise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
-            WarpNoise.SetFrequency(NoiseSettings.DomainWarpFrequency);
-        }
+        FCosmicNoiseEvaluator Evaluator(NoiseSettings);
 
         const int32 VertexCount = BaseVertices.Num();
 
@@ -159,161 +59,16 @@ public:
             FVector WorldPos = ComponentTransform.TransformPosition(BaseVertices[i]);
             FVector NoiseDir = (WorldPos - PlanetCenter).GetSafeNormal();
 
-            float X = NoiseDir.X;
-            float Y = NoiseDir.Y;
-            float Z = NoiseDir.Z;
+            float FinalHeight;
+            FLinearColor FinalColor;
 
-            // Aplicar Domain Warp
-            if (NoiseSettings.bUseDomainWarp)
-            {
-                float WarpX = WarpNoise.GetNoise(X, Y, Z) * NoiseSettings.DomainWarpStrength;
-                float WarpY = WarpNoise.GetNoise(X + 31.7f, Y + 17.3f, Z + 47.1f) * NoiseSettings.DomainWarpStrength;
-                float WarpZ = WarpNoise.GetNoise(X + 59.2f, Y + 11.8f, Z + 23.4f) * NoiseSettings.DomainWarpStrength;
-
-                X += WarpX;
-                Y += WarpY;
-                Z += WarpZ;
-            }
-
-            float RawHum = HumidityNoise.GetNoise(NoiseDir.X, NoiseDir.Y, NoiseDir.Z);
-            float BaseHum = (RawHum + 1.0f) * 0.5f + NoiseSettings.HumidityOffset;
-            float FinalHum = FMath::Clamp((BaseHum - 0.5f) * NoiseSettings.HumidityContrast + 0.5f, 0.0f, 1.0f);
-           
-            float Latitude = FMath::Abs(NoiseDir.Z); // 0 en el ecuador, 1 en los polos
-            float BaseTemp = 1.0f - (Latitude * NoiseSettings.LatitudeEffect);
-            float TempVariance = TempNoise.GetNoise(NoiseDir.X, NoiseDir.Y, NoiseDir.Z) * 0.2f;
-
-            float FinalTemp = FMath::Clamp(BaseTemp + TempVariance, 0.0f, 1.0f);
-
-            int32 ClosestBiomeIdx = -1;
-            int32 SecondClosestBiomeIdx = -1;
-            float MinDistSq = UE_BIG_NUMBER;
-            float SecondMinDistSq = UE_BIG_NUMBER;
-
-            for (int j = 0; j < NoiseSettings.Biomes.Num(); j++)
-            {
-                float dTemp = FinalTemp - NoiseSettings.Biomes[j].TargetTemperature;
-                float dHum = FinalHum - NoiseSettings.Biomes[j].TargetHumidity;
-
-                // Distancia al cuadrado (más rápido que hacer FMath::Sqrt)
-                float DistSq = (dTemp * dTemp) + (dHum * dHum);
-
-                if (DistSq < MinDistSq)
-                {
-                    SecondMinDistSq = MinDistSq;
-                    SecondClosestBiomeIdx = ClosestBiomeIdx;
-                    MinDistSq = DistSq;
-                    ClosestBiomeIdx = j;
-                }
-                else if (DistSq < SecondMinDistSq)
-                {
-                    SecondMinDistSq = DistSq;
-                    SecondClosestBiomeIdx = j;
-                }
-            }
-            float FinalHeight = 0.0f;
-
-            if (ClosestBiomeIdx != -1) {
-                float Height1 = 0.0f;
-                for (int k = 0; k < NoiseSettings.Biomes[ClosestBiomeIdx].NoiseLayers.Num(); k++) {
-                    Height1 += BiomeNoises[ClosestBiomeIdx][k].GetNoise(X, Y, Z) * NoiseSettings.Biomes[ClosestBiomeIdx].NoiseLayers[k].Amplitude;
-                }
-
-                if (SecondClosestBiomeIdx != -1)
-                {
-                    // 2. Calcular altura del bioma secundario
-                    float Height2 = 0.0f;
-                    for (int32 l = 0; l < NoiseSettings.Biomes[SecondClosestBiomeIdx].NoiseLayers.Num(); l++) {
-                        Height2 += BiomeNoises[SecondClosestBiomeIdx][l].GetNoise(X, Y, Z) * NoiseSettings.Biomes[SecondClosestBiomeIdx].NoiseLayers[l].Amplitude;
-                    }
-
-                    // 3. Calcular la fuerza (peso) de cada uno
-                    // Extraemos raíces cuadradas solo ahora al final para la matemática precisa del peso
-                    float Dist1 = FMath::Sqrt(MinDistSq);
-                    float Dist2 = FMath::Sqrt(SecondMinDistSq);
-
-                    // Evitar división por cero si estamos exactamente en el punto ideal
-                    if (Dist1 < 0.0001f) Dist1 = 0.0001f;
-                    if (Dist2 < 0.0001f) Dist2 = 0.0001f;
-
-                    float BlendSharpness = 5.0f;
-
-                    float Weight1 = 1.0f / FMath::Pow(Dist1, BlendSharpness);
-                    float Weight2 = 1.0f / FMath::Pow(Dist2, BlendSharpness);
-                    float SumWeights = Weight1 + Weight2;
-
-                    Weight1 /= SumWeights;
-                    Weight2 /= SumWeights;
-
-                    // 4. Mezclar las dos alturas
-                    FinalHeight = (Height1 * Weight1) + (Height2 * Weight2);
-                }
-                else
-                {
-                    // Si solo hay 1 bioma en todo el array
-                    FinalHeight = Height1;
-                }
-            }
-
-            if (NoiseSettings.bIsCraterPlanet)
-            {
-                // Distancia al centro de la celda Voronoi
-                float CellDistance = CraterNoise.GetNoise(X, Y, Z);
-                float CraterRadius = NoiseSettings.CraterRadiusMultiplier;
-
-                CellDistance = (CellDistance + 1.0f) * 0.5f;
-
-                if (CellDistance < CraterRadius * 1.3f)
-                {
-                    float t = CellDistance / CraterRadius;// 0 centro, 1 borde
-
-                    float craterHeight = 0.0f;
-
-                    // CAVIDAD 
-                    if (t < 1.0f)
-                    {
-                        float floorStart = NoiseSettings.CraterFloorHeight; // 0.0 - 1.0
-
-                        float bowl = 0.0f;
-
-                        if (t < floorStart)
-                        {
-                            // zona plana del cráter
-                            bowl = 1.0f;
-                        }
-                        else
-                        {
-                            // pared del cráter
-                            float wallT = (t - floorStart) / (1.0f - floorStart);
-                            wallT = FMath::SmoothStep(0.0f, 1.0f, wallT);
-                            bowl = 1.0f - wallT;
-                            bowl *= bowl;
-                        }
-
-                        craterHeight -= bowl * NoiseSettings.CraterDepth;
-                    }
-
-                    // RIM 
-                    float rimWidth = 0.15f;
-
-                    float rim = FMath::Exp(-FMath::Pow((t - 1.0f) / rimWidth, 2.0f) * NoiseSettings.CraterRimSharpness);
-
-                    craterHeight += rim * NoiseSettings.CraterDepth * NoiseSettings.CraterRimHeight;
-
-                    FinalHeight += craterHeight;
-                }
-            }
+            Evaluator.EvaluatePoint(NoiseDir, FinalHeight, FinalColor);
 
             // Calcular posición final del vértice
             CalculatedVertices[i] = BaseVertices[i] + (BaseNormals[i] * FinalHeight);
 
-            // Penalización por altitud (usando FinalHeight)
-            float AltitudePenalty = FMath::Clamp(FinalHeight / MaxPossibleHeight, 0.0f, 1.0f);
-
-            float VisualTemp = FMath::Clamp(FinalTemp - (AltitudePenalty * NoiseSettings.AltitudeTemperaturePenalty), 0.0f, 1.0f);
-            float BiomeID = (ClosestBiomeIdx != -1) ? ((float)ClosestBiomeIdx / 255.0f) : 0.0f;
             // Guardar colores
-            CalculatedColors[i] = FLinearColor(AltitudePenalty, VisualTemp, FinalHum, BiomeID);
+            CalculatedColors[i] = FinalColor;
             
         }
     }
