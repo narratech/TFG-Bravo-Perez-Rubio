e87bdd62b46872758dbbb6eb3cc415b3bc63de00
JaviBravoPerucho <125208949+JaviBravoPerucho@users.noreply.github.com>
2026-04-14 10:35:31 +0200

    Factoria de System Generator

---
+++ Source/CosmicArchitectEditor/Public/CosmicSystemGeneratorFactory.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/BlueprintFactory.h"
+#include "CosmicSystemGeneratorFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COSMICARCHITECTEDITOR_API UCosmicSystemGeneratorFactory : public UBlueprintFactory
+{
+	GENERATED_BODY()
+	
+public:
+
+	UCosmicSystemGeneratorFactory();
+
+	// El nombre que aparecerá en el menú del clic derecho
+	virtual FText GetDisplayName() const override;
+
+	virtual uint32 GetMenuCategories() const override;
+};
