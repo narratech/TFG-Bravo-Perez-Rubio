5cd9b5b18dd931c69afa2a8a86ff60aeeacbeb76
JaviBravoPerucho <125208949+JaviBravoPerucho@users.noreply.github.com>
2026-05-12 16:43:23 +0200

    Package fuera

--- CosmicArchitectPackage/Source/CosmicArchitectRuntime/Private/CosmicCameraBridge.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CosmicCameraBridge.h"
-
-
-FVector FCosmicCameraBridge::CameraLocation = FVector::ZeroVector;
-FRotator FCosmicCameraBridge::CameraRotation = FRotator::ZeroRotator;
